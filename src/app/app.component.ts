f1af4d7610445ff4cb7365775a61babb12a84ebd
shifa <shifa7446@gmail.com>
2024-11-11 15:42:04 +0600

    Update src folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'omti_app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.loadScript();
+  }
+  public loadScript(): void {
+    const script = document.createElement('script');
+    script.src = '../assets/js/main_other.js';
+    script.onload = () => { // Initialize any functions defined in the external JS file
+    };
+    document.body.appendChild(script);
+  }
 }
