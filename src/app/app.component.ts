6a3fea4d18f99c2d14b38040d2a559d18395a606
shifa <shifa7446@gmail.com>
2024-11-11 15:55:07 +0600

    template update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+
   ngOnInit(): void {
-    this.loadScript();
+     this.loadScript();
   }
   public loadScript(): void {
     const script = document.createElement('script');
-    script.src = '../assets/js/main_other.js';
-    script.onload = () => { // Initialize any functions defined in the external JS file
+    script.src = 'assets/js/main.js';
+    script.onload = () => {
+      // Initialize any functions defined in the external JS file
     };
     document.body.appendChild(script);
   }
